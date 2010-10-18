9e7cddf2f2afb91002f80bd6eb7ca2589f68e8fb
ccc.raab <ccc.raab@gmail.com>
2010-10-18 11:26:47 +0000

    integration gyro without float

--- CorvusM3_FC/CorvusM3_Firmware/trunk/filterhh.c
+++ CorvusM3_FC/CorvusM3_Firmware/trunk/filterhh.c
@@ -92,20 +92,37 @@ void getCopterAnglesFilterHH(vs32 * gyroAngle, vs32 * copterAngle)
 // ADC * 3,3 / 4095 / 2000 * 1000 
 void getGyroAnglesFilterHH(vs32 * gyroAngle, vs32 * gyroRawValues)
 {
+	u8 i;
 	//#################
 	//http://tom.pycke.be/mav/70/gyroscope-to-roll-pitch-and-yaw
 	//try runge-kutta integration http://de.wikipedia.org/wiki/Runge-Kutta-Verfahren
 	//integration(i) = integration(i-1) + 1/6 ( vali-3 + 2 vali-2 + 2 vali-1 + vali)
-	vs32 actualGyroRawValues[3];
-	actualGyroRawValues[X] = (vs32) (gyroRawValues[X] * ( 3.3 / 4095.0 / 2000.0 ) * parameter[PARA_GYRO_X_90] * 100);
-	actualGyroRawValues[Y] = (vs32) (gyroRawValues[Y] * ( 3.3 / 4095.0 / 2000.0 ) * parameter[PARA_GYRO_Y_90] * 100);
-	actualGyroRawValues[Z] = (vs32) (gyroRawValues[Z] * ( 3.3 / 4095.0 / 2000.0 ) * parameter[PARA_GYRO_Z_90] * 100);
 	
+	vs32 actualGyroRawValues[3];
+	//actualGyroRawValues[X] = (vs32) (gyroRawValues[X] * ( 3.3 / 4095.0 / 2000.0 ) * parameter[PARA_GYRO_X_90] * 100);
+	//actualGyroRawValues[Y] = (vs32) (gyroRawValues[Y] * ( 3.3 / 4095.0 / 2000.0 ) * parameter[PARA_GYRO_Y_90] * 100);
+	//actualGyroRawValues[Z] = (vs32) (gyroRawValues[Z] * ( 3.3 / 4095.0 / 2000.0 ) * parameter[PARA_GYRO_Z_90] * 100);
+	
+	actualGyroRawValues[X] = gyroRawValues[X] * parameter[PARA_GYRO_X_90] / 24818;
+	actualGyroRawValues[Y] = gyroRawValues[Y] * parameter[PARA_GYRO_Y_90] / 24818;
+	actualGyroRawValues[Z] = gyroRawValues[Z] * parameter[PARA_GYRO_Z_90] / 24818;
+	
+	/*
 	gyroAngle[X] -= (gyroRawValues3[X] + 2 * gyroRawValues2[X] + 2 * gyroRawValues1[X] + actualGyroRawValues[X]) / 6;
 	gyroAngle[Y] -= (gyroRawValues3[Y] + 2 * gyroRawValues2[Y] + 2 * gyroRawValues1[Y] + actualGyroRawValues[Y]) / 6;
 	gyroAngle[Z] -= (gyroRawValues3[Z] + 2 * gyroRawValues2[Z] + 2 * gyroRawValues1[Z] + actualGyroRawValues[Z]) / 6; 
+	*/
 	
-	u8 i;
+	//vs32 temp = (gyroRawValues3[X] + 2 * gyroRawValues2[X] + 2 * gyroRawValues1[X] + actualGyroRawValues[X]) / 6;
+	
+	gyroAngle[X] -= actualGyroRawValues[X];
+	gyroAngle[Y] -= (gyroRawValues3[Y] + 2 * gyroRawValues2[Y] + 2 * gyroRawValues1[Y] + actualGyroRawValues[Y]) / 6;
+	gyroAngle[Z] -= actualGyroRawValues[Z]; 
+	
+	//char x [80];
+	//sprintf(x,"%d\r\n",actualGyroRawValues[X]);
+	//print_uart1(x);
+	
 	for (i = 0; i < 3; i++)
 	{
 		gyroRawValues3[i] = gyroRawValues2[i];
@@ -121,9 +138,6 @@ void getGyroAnglesFilterHH(vs32 * gyroAngle, vs32 * gyroRawValues)
     gyroAngle[Z] -= (vs32) (gyroRawValues[Z] * ( 3.3 / 4095.0 / 2000.0 ) * parameter[PARA_GYRO_Z_90] * 100);
 	*/
 	
-	//char x [80];
-	//sprintf(x,"gyro raw value:%d:%d:%d\r\n",gyroAngle[X],gyroRawValues[X],gyroAngle[Z]);
-	//print_uart1(x);
 	// overrun
 	//u8 i;
 	for (i = 0; i < 3; i++)
