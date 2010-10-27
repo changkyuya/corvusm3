bf811f8b970642370cf735a0a5d79c96b26ab7ac
ccc.raab <ccc.raab@gmail.com>
2010-10-27 08:04:37 +0000

    include cockpit controls

--- CorvusM3_FC/CorvusM3_Set/trunk/Properties/Resources.Designer.cs
+++ CorvusM3_FC/CorvusM3_Set/trunk/Properties/Resources.Designer.cs
@@ -59,5 +59,138 @@ namespace CorvusM3.Properties {
                 resourceCulture = value;
             }
         }
+        
+        public static System.Drawing.Bitmap AirSpeedIndicator_Background {
+            get {
+                object obj = ResourceManager.GetObject("AirSpeedIndicator_Background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap AirSpeedNeedle {
+            get {
+                object obj = ResourceManager.GetObject("AirSpeedNeedle", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap Altimeter_Background {
+            get {
+                object obj = ResourceManager.GetObject("Altimeter_Background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap Bandeau_Dérouleur {
+            get {
+                object obj = ResourceManager.GetObject("Bandeau_Dérouleur", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap HeadingIndicator_Aircraft {
+            get {
+                object obj = ResourceManager.GetObject("HeadingIndicator_Aircraft", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap HeadingIndicator_Background {
+            get {
+                object obj = ResourceManager.GetObject("HeadingIndicator_Background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap HeadingWeel {
+            get {
+                object obj = ResourceManager.GetObject("HeadingWeel", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap Horizon_Background {
+            get {
+                object obj = ResourceManager.GetObject("Horizon_Background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap Horizon_GroundSky {
+            get {
+                object obj = ResourceManager.GetObject("Horizon_GroundSky", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap Indicator_Background {
+            get {
+                object obj = ResourceManager.GetObject("Indicator_Background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap LongNeedleAltimeter {
+            get {
+                object obj = ResourceManager.GetObject("LongNeedleAltimeter", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap Maquette_Avion {
+            get {
+                object obj = ResourceManager.GetObject("Maquette_Avion", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap SmallNeedleAltimeter {
+            get {
+                object obj = ResourceManager.GetObject("SmallNeedleAltimeter", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap TurnCoordinator_Background {
+            get {
+                object obj = ResourceManager.GetObject("TurnCoordinator_Background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap TurnCoordinatorAircraft {
+            get {
+                object obj = ResourceManager.GetObject("TurnCoordinatorAircraft", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap TurnCoordinatorBall {
+            get {
+                object obj = ResourceManager.GetObject("TurnCoordinatorBall", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap TurnCoordinatorMarks {
+            get {
+                object obj = ResourceManager.GetObject("TurnCoordinatorMarks", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap VerticalSpeedIndicator_Background {
+            get {
+                object obj = ResourceManager.GetObject("VerticalSpeedIndicator_Background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        public static System.Drawing.Bitmap VerticalSpeedNeedle {
+            get {
+                object obj = ResourceManager.GetObject("VerticalSpeedNeedle", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
