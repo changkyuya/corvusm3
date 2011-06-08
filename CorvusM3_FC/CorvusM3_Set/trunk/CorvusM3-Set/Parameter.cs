01438d23249605faf33dac9ff9bb65fe0fe2134b
ccc.raab <ccc.raab@dc328c70-bfae-11de-a0e9-83d03c287164>
2011-06-08 17:43:21 +0000

    smal changes

--- CorvusM3_FC/CorvusM3_Set/trunk/CorvusM3-Set/Parameter.cs
+++ CorvusM3_FC/CorvusM3_Set/trunk/CorvusM3-Set/Parameter.cs
@@ -63,63 +63,63 @@ namespace CorvusM3
             { 
                 string [] data = e.Dataline.Split(';');
                 data[0] = data[0].Substring(1);
-                parameter[Convert.ToInt16(data[0])] = float.Parse(data[1]);
-                serialComm.setParameter(Convert.ToInt16(data[0]), float.Parse(data[1]));
-                propGrid.Refresh();
+                parameter[Convert.ToInt16(data[0])] = float.Parse(data[1].Replace('.', ','));
+                serialComm.setParameter(Convert.ToInt16(data[0]), float.Parse(data[1].Replace('.',',')));
+                
             }
         }
 
 
 
-        [CategoryAttribute("PID Stable"), DisplayName("ROLL P"), DescriptionAttribute("Default 4.0")]
+        [CategoryAttribute("PID Stable"), DisplayName("ROLL P [0]"), DescriptionAttribute("Default 4.0")]
         public float KP_QUAD_ROLL
         {
             set { parameter[0] = value; serialComm.setParameter(0, value); }
             get { return parameter[0]; }
         }
-        [CategoryAttribute("PID Stable"), DisplayName("ROLL I"), DescriptionAttribute("Default 0.15")]
+        [CategoryAttribute("PID Stable"), DisplayName("ROLL I [1]"), DescriptionAttribute("Default 0.15")]
         public float KI_QUAD_ROLL
         {
             set { parameter[1] = value; serialComm.setParameter(1, value); }
             get { return parameter[1]; }
         }
-        [CategoryAttribute("PID Stable"), DisplayName("ROLL RATE P"), DescriptionAttribute("Default 1.2")]
+        [CategoryAttribute("PID Stable"), DisplayName("ROLL RATE P [2]"), DescriptionAttribute("Default 1.2")]
         public float STABLE_MODE_KP_RATE_ROLL
         {
             set { parameter[2] = value; serialComm.setParameter(2, value); }
             get { return parameter[2]; }
         }
-        [CategoryAttribute("PID Stable"), DisplayName("NICK P"), DescriptionAttribute("Default 4.0")]
+        [CategoryAttribute("PID Stable"), DisplayName("NICK P [3]"), DescriptionAttribute("Default 4.0")]
         public float KP_QUAD_NICK
         {
             set { parameter[3] = value; serialComm.setParameter(3, value); }
             get { return parameter[3]; }
         }
-        [CategoryAttribute("PID Stable"), DisplayName("NICK I"), DescriptionAttribute("Default 0.15")]
+        [CategoryAttribute("PID Stable"), DisplayName("NICK I [4]"), DescriptionAttribute("Default 0.15")]
         public float KI_QUAD_NICK
         {
             set { parameter[4] = value; serialComm.setParameter(4, value); }
             get { return parameter[4]; }
         }
-        [CategoryAttribute("PID Stable"), DisplayName("NICK RATE P"), DescriptionAttribute("Default 1.2")]
+        [CategoryAttribute("PID Stable"), DisplayName("NICK RATE P [5]"), DescriptionAttribute("Default 1.2")]
         public float STABLE_MODE_KP_RATE_NICK
         {
             set { parameter[5] = value; serialComm.setParameter(5, value); }
             get { return parameter[5]; }
         }
-        [CategoryAttribute("PID Stable"), DisplayName("YAW P"), DescriptionAttribute("Default 4.0")]
+        [CategoryAttribute("PID Stable"), DisplayName("YAW P [6]"), DescriptionAttribute("Default 4.0")]
         public float KP_QUAD_YAW
         {
             set { parameter[6] = value; serialComm.setParameter(6, value); }
             get { return parameter[6]; }
         }
-        [CategoryAttribute("PID Stable"), DisplayName("YAW I"), DescriptionAttribute("Default 0.15")]
+        [CategoryAttribute("PID Stable"), DisplayName("YAW I [7]"), DescriptionAttribute("Default 0.15")]
         public float KI_QUAD_YAW
         {
             set { parameter[7] = value; serialComm.setParameter(7, value); }
             get { return parameter[7]; }
         }
-        [CategoryAttribute("PID Stable"), DisplayName("YAW RATE P"), DescriptionAttribute("Default 1.2")]
+        [CategoryAttribute("PID Stable"), DisplayName("YAW RATE P [8]"), DescriptionAttribute("Default 1.2")]
         public float STABLE_MODE_KP_RATE_YAW
         {
             set { parameter[8] = value; serialComm.setParameter(8, value); }
@@ -127,19 +127,19 @@ namespace CorvusM3
         }
 
 
-        [CategoryAttribute("Sensors"), DisplayName("ACC offset X"), DescriptionAttribute("Default 2057")]
+        [CategoryAttribute("Sensors"), DisplayName("ACC offset X [10]"), DescriptionAttribute("Default 2057")]
         public float acc_offset_x
         {
             set { parameter[10] = value; serialComm.setParameter(10, value); }
             get { return parameter[10]; }
         }
-        [CategoryAttribute("Sensors"), DisplayName("ACC offset Y"), DescriptionAttribute("Default 2044")]
+        [CategoryAttribute("Sensors"), DisplayName("ACC offset Y [11]"), DescriptionAttribute("Default 2044")]
         public float acc_offset_y
         {
             set { parameter[11] = value; serialComm.setParameter(11, value); }
             get { return parameter[11]; }
         }
-        [CategoryAttribute("Sensors"), DisplayName("ACC offset Z"), DescriptionAttribute("Default 2052")]
+        [CategoryAttribute("Sensors"), DisplayName("ACC offset Z [12]"), DescriptionAttribute("Default 2052")]
         public float acc_offset_z
         {
             set { parameter[12] = value; serialComm.setParameter(12, value); }
@@ -147,25 +147,25 @@ namespace CorvusM3
         }
 
 
-        [CategoryAttribute("DCM"), DisplayName("ROLLNICK P"), DescriptionAttribute("Default 0.0014")]
+        [CategoryAttribute("DCM"), DisplayName("ROLLNICK P [13]"), DescriptionAttribute("Default 0.0014")]
         public float Kp_ROLLNICK
         {
             set { parameter[13] = value; serialComm.setParameter(13, value); }
             get { return parameter[13]; }
         }
-        [CategoryAttribute("DCM"), DisplayName("ROLLNICK I"), DescriptionAttribute("Default 0.00000015")]
+        [CategoryAttribute("DCM"), DisplayName("ROLLNICK I [14]"), DescriptionAttribute("Default 0.00000015")]
         public float Ki_ROLLNICK
         {
             set { parameter[14] = value; serialComm.setParameter(14, value); }
             get { return parameter[14]; }
         }
-        [CategoryAttribute("DCM"), DisplayName("YAW P"), DescriptionAttribute("Default 1.0")]
+        [CategoryAttribute("DCM"), DisplayName("YAW P [15]"), DescriptionAttribute("Default 1.0")]
         public float Kp_YAW
         {
             set { parameter[15] = value; serialComm.setParameter(15, value); }
             get { return parameter[15]; }
         }
-        [CategoryAttribute("DCM"), DisplayName("YAW I"), DescriptionAttribute("Default 0.00002")]
+        [CategoryAttribute("DCM"), DisplayName("YAW I [16]"), DescriptionAttribute("Default 0.00002")]
         public float Ki_YAW
         {
             set { parameter[16] = value; serialComm.setParameter(16, value); }
@@ -174,61 +174,61 @@ namespace CorvusM3
 
 
 
-        [CategoryAttribute("PID HH"), DisplayName("\tStick Factor HH"), DescriptionAttribute("Default 0.32")]
+        [CategoryAttribute("PID HH"), DisplayName("\tStick Factor HH [17]"), DescriptionAttribute("Default 0.32")]
         public float XMITFACTOR
         {
             set { parameter[17] = value; serialComm.setParameter(17, value); }
             get { return parameter[17]; }
         }
-        [CategoryAttribute("PID HH"), DisplayName("ROLL P"), DescriptionAttribute("Default 1.95")]
+        [CategoryAttribute("PID HH"), DisplayName("ROLL P [18]"), DescriptionAttribute("Default 1.95")]
         public float Kp_RateRoll
         {
             set { parameter[18] = value; serialComm.setParameter(18, value); }
             get { return parameter[18]; }
         }
-        [CategoryAttribute("PID HH"), DisplayName("ROLL I"), DescriptionAttribute("Default 0.0")]
+        [CategoryAttribute("PID HH"), DisplayName("ROLL I [19]"), DescriptionAttribute("Default 0.0")]
         public float Ki_RateRoll
         {
             set { parameter[19] = value; serialComm.setParameter(19, value); }
             get { return parameter[19]; }
         }
-        [CategoryAttribute("PID HH"), DisplayName("ROLL D"), DescriptionAttribute("Default 0.0")]
+        [CategoryAttribute("PID HH"), DisplayName("ROLL D [20]"), DescriptionAttribute("Default 0.0")]
         public float Kd_RateRoll
         {
             set { parameter[20] = value; serialComm.setParameter(20, value); }
             get { return parameter[20]; }
         }
-        [CategoryAttribute("PID HH"), DisplayName("NICK P"), DescriptionAttribute("Default 1.95")]
+        [CategoryAttribute("PID HH"), DisplayName("NICK P [21]"), DescriptionAttribute("Default 1.95")]
         public float Kp_RateNick
         {
             set { parameter[21] = value; serialComm.setParameter(21, value); }
             get { return parameter[21]; }
         }
-        [CategoryAttribute("PID HH"), DisplayName("NICK I"), DescriptionAttribute("Default 0.0")]
+        [CategoryAttribute("PID HH"), DisplayName("NICK I [22]"), DescriptionAttribute("Default 0.0")]
         public float Ki_RateNick
         {
             set { parameter[22] = value; serialComm.setParameter(22, value); }
             get { return parameter[22]; }
         }
-        [CategoryAttribute("PID HH"), DisplayName("NICK D"), DescriptionAttribute("Default 0.0")]
+        [CategoryAttribute("PID HH"), DisplayName("NICK D [23]"), DescriptionAttribute("Default 0.0")]
         public float Kd_RateNick
         {
             set { parameter[23] = value; serialComm.setParameter(23, value); }
             get { return parameter[23]; }
         }
-        [CategoryAttribute("PID HH"), DisplayName("YAW P"), DescriptionAttribute("Default 3.2")]
+        [CategoryAttribute("PID HH"), DisplayName("YAW P [24]"), DescriptionAttribute("Default 3.2")]
         public float Kp_RateYaw
         {
             set { parameter[24] = value; serialComm.setParameter(24, value); }
             get { return parameter[24]; }
         }
-        [CategoryAttribute("PID HH"), DisplayName("YAW I"), DescriptionAttribute("Default 0.0")]
+        [CategoryAttribute("PID HH"), DisplayName("YAW I [25]"), DescriptionAttribute("Default 0.0")]
         public float Ki_RateYaw
         {
             set { parameter[25] = value; serialComm.setParameter(25, value); }
             get { return parameter[25]; }
         }
-        [CategoryAttribute("PID HH"), DisplayName("YAW D"), DescriptionAttribute("Default 0.0")]
+        [CategoryAttribute("PID HH"), DisplayName("YAW D [26]"), DescriptionAttribute("Default 0.0")]
         public float Kd_RateYaw
         {
             set { parameter[26] = value; serialComm.setParameter(26, value); }
@@ -236,45 +236,45 @@ namespace CorvusM3
         }
 
 
-        [CategoryAttribute("Remote"), DisplayName("ROLL Center"), DescriptionAttribute("Default 1500")]
+        [CategoryAttribute("Remote"), DisplayName("ROLL Center [27]"), DescriptionAttribute("Default 1500")]
         public float CHROLL_MID
         {
             set { parameter[27] = value; serialComm.setParameter(27, value); }
             get { return parameter[27]; }
         }
-        [CategoryAttribute("Remote"), DisplayName("NICK Center"), DescriptionAttribute("Default 1500")]
+        [CategoryAttribute("Remote"), DisplayName("NICK Center [28]"), DescriptionAttribute("Default 1500")]
         public float CHNICK_MID
         {
             set { parameter[28] = value; serialComm.setParameter(28, value); }
             get { return parameter[28]; }
         }
-        [CategoryAttribute("Remote"), DisplayName("YAW Center"), DescriptionAttribute("Default 1500")]
+        [CategoryAttribute("Remote"), DisplayName("YAW Center [29]"), DescriptionAttribute("Default 1500")]
         public float CHYAW_MID
         {
             set { parameter[29] = value; serialComm.setParameter(29, value); }
             get { return parameter[29]; }
         }
-        [CategoryAttribute("Remote"), DisplayName("PITCH min"), DescriptionAttribute("Default 1100")]
+        [CategoryAttribute("Remote"), DisplayName("PITCH min [30]"), DescriptionAttribute("Default 1100")]
         public float MIN_PITCH
         {
             set { parameter[30] = value; serialComm.setParameter(30, value); }
             get { return parameter[30]; }
         }
 
-        [CategoryAttribute("Basis"), DisplayName("Batterie warning Volt"), DescriptionAttribute("Default 10")]
+        [CategoryAttribute("Basis"), DisplayName("Batterie warning Volt [31]"), DescriptionAttribute("Default 10")]
         public float BAT_WARNING
         {
             set { parameter[31] = value; serialComm.setParameter(31, value); }
             get { return parameter[31]; }
         }
 
-        [CategoryAttribute("Remote"), DisplayName("Stick to Angle Divisor ROLL/NICK"), DescriptionAttribute("Default 5.0")]
+        [CategoryAttribute("Remote"), DisplayName("Stick to Angle Divisor ROLL/NICK [32]"), DescriptionAttribute("Default 5.0")]
         public float STICK_TO_ANGLE_FACTOR
         {
             set { parameter[32] = value; serialComm.setParameter(32, value); }
             get { return parameter[32]; }
         }
-        [CategoryAttribute("Remote"), DisplayName("Stick to Angle Divisor YAW"), DescriptionAttribute("Default 1150.0")]
+        [CategoryAttribute("Remote"), DisplayName("Stick to Angle Divisor YAW [33]"), DescriptionAttribute("Default 1150.0")]
         public float YAW_STICK_TO_ANGLE_FACTOR
         {
             set { parameter[33] = value; serialComm.setParameter(33, value); }
